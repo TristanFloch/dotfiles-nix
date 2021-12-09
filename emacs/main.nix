c597f3c403d15cc320148236c843aa78031eac0b
Tristan FLOCH <tristan.floch@epita.fr>
2021-12-09 20:48:56 +0100

    emacs basic

--- emacs/main.nix
+++ emacs/main.nix
@@ -1,19 +1,23 @@
 { config, lib, pkgs, ... }:
 
+# let
+#   doom-emacs = pkgs.callPackage (builtins.fetchTarball {
+#     url = https://github.com/vlaci/nix-doom-emacs/archive/master.tar.gz;
+#   }) {
+#     doomPrivateDir = ./doom.d;
+#   };
+# in {
+#   home.file.".emacs.d/init.el".text = ''
+#     (load "default.el")
+#   '';
+#   programs.doom-emacs = {
+#     enable = true;
+#     # emacsPackage = pkgs.emacsGcc;
+#   };
+# }
+
 {
-  let
-    doom-emacs = pkgs.callPackage (builtins.fetchTarball {
-      url = https://github.com/vlaci/nix-doom-emacs/archive/master.tar.gz;
-    }) {
-      doomPrivateDir = ./doom.d
-    };
-  in {
-    home.file.".emacs.d/init.el".text = ''
-      (load "default.el")
-    '';
-    programs.doom-emacs = {
-      enable = true;
-      emacsPackage = pkgs.emacsGcc;
-    }
-  }
+  programs.emacs = {
+    enable = true;
+  };
 }
