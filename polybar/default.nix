2375e31ead0e797944968f84fe3ffe094bba21ec
Tristan FLOCH <tristan.floch@epita.fr>
2022-01-02 11:35:34 +0100

    split polybar config across multiple files

--- polybar/default.nix
+++ polybar/default.nix
@@ -4,11 +4,14 @@
   services.polybar = {
     enable = true;
     package = pkgs.polybarFull;
-    config = ./config;
+    config = ./config.ini;
     script = ''
       for m in $(polybar --list-monitors | ${pkgs.coreutils}/bin/cut -d":" -f1); do
-        MONITOR=$m polybar mainbar-i3 &
+        MONITOR=$m polybar main &
       done
     '';
   };
+
+  home.file.".config/polybar/colors.ini".source = ./colors.ini;
+  home.file.".config/polybar/modules.ini".source = ./modules.ini;
 }
