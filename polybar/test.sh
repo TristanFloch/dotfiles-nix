42857213315e868f6efa21de7a41878261001306
Tristan FLOCH <tristan.floch@epita.fr>
2022-01-03 20:32:03 +0100

    script to test polybar

---
+++ polybar/test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+BARNAME=main
+
+killall -rq 'polybar'
+
+# if [ "$(systemctl --user status polybar.service)" -ne 0 ]; then
+#     systemctl --user stop polybar.service
+# fi
+
+ln -sf ~/.config/nixpkgs/polybar/config.ini ~/.config/polybar/config
+ln -sf ~/.config/nixpkgs/polybar/modules.ini ~/.config/polybar/modules.ini
+ln -sf ~/.config/nixpkgs/polybar/colors.ini ~/.config/polybar/colors.ini
+
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload "$BARNAME" -c ~/.config/polybar/config &
+done
