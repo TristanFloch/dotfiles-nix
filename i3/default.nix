f4c965f40766f2a5344840188113ee40640ff214
Tristan FLOCH <tristan.floch@epita.fr>
2022-01-16 15:24:49 +0100

    style(i3): nix format

--- i3/default.nix
+++ i3/default.nix
@@ -8,7 +8,7 @@
       package = pkgs.i3-gaps;
       config = {
         modifier = "Mod4";
-        bars = [];
+        bars = [ ];
         terminal = "alacritty";
         defaultWorkspace = "workspace number 1";
         workspaceAutoBackAndForth = true;
@@ -30,20 +30,20 @@
         };
 
         modes = {
-           resize = {
-             l = "resize shrink width 5 px or 5 ppt";
-             h = "resize grow width 5 px or 5 ppt";
-             j = "resize shrink height 5 px or 5 ppt";
-             k = "resize grow height 5 px or 5 ppt";
+          resize = {
+            l = "resize shrink width 5 px or 5 ppt";
+            h = "resize grow width 5 px or 5 ppt";
+            j = "resize shrink height 5 px or 5 ppt";
+            k = "resize grow height 5 px or 5 ppt";
 
-             Right = "resize shrink width 10 px or 10 ppt";
-             Left = "resize grow width 10 px or 10 ppt";
-             Down = "resize shrink height 10 px or 10 ppt";
-             Up = "resize grow height 10 px or 10 ppt";
+            Right = "resize shrink width 10 px or 10 ppt";
+            Left = "resize grow width 10 px or 10 ppt";
+            Down = "resize shrink height 10 px or 10 ppt";
+            Up = "resize grow height 10 px or 10 ppt";
 
-             Escape = "mode default";
-             Return = "mode default";
-           };
+            Escape = "mode default";
+            Return = "mode default";
+          };
         };
 
         startup = [
@@ -59,62 +59,64 @@
 
         window = {
           border = 1;
-          commands = [ { command = "move to workspace $ws8"; criteria = { class = "Spotify"; } ; } ];
+          commands = [{ command = "move to workspace $ws8"; criteria = { class = "Spotify"; }; }];
         };
 
         floating = {
           border = 1;
         };
 
-        keybindings = let
-          mod = config.modifier;
-        in lib.mkOptionDefault {
-          "${mod}+Shift+q" = "kill";
-          "${mod}+d" = "exec rofi -show drun";
-          "${mod}+Shift+e" = "exec xfce4-session-logout";
+        keybindings =
+          let
+            mod = config.modifier;
+          in
+          lib.mkOptionDefault {
+            "${mod}+Shift+q" = "kill";
+            "${mod}+d" = "exec rofi -show drun";
+            "${mod}+Shift+e" = "exec xfce4-session-logout";
 
-          "XF86MonBrightnessUp" = "exec brightnessctl -c backlight set +10%";
-          "XF86MonBrightnessDown" = "exec brightnessctl -c backlight set 10%-";
+            "XF86MonBrightnessUp" = "exec brightnessctl -c backlight set +10%";
+            "XF86MonBrightnessDown" = "exec brightnessctl -c backlight set 10%-";
 
-          "${mod}+n" = "border normal";
+            "${mod}+n" = "border normal";
 
-          "${mod}+h" = "focus left";
-          "${mod}+j" = "focus down";
-          "${mod}+k" = "focus up";
-          "${mod}+l" = "focus right";
+            "${mod}+h" = "focus left";
+            "${mod}+j" = "focus down";
+            "${mod}+k" = "focus up";
+            "${mod}+l" = "focus right";
 
-          "${mod}+Left" = "focus left";
-          "${mod}+Down" = "focus down";
-          "${mod}+Up" = "focus up";
-          "${mod}+Right" = "focus right";
+            "${mod}+Left" = "focus left";
+            "${mod}+Down" = "focus down";
+            "${mod}+Up" = "focus up";
+            "${mod}+Right" = "focus right";
 
-          "${mod}+Shift+h" = "move left";
-          "${mod}+Shift+j" = "move down";
-          "${mod}+Shift+k" = "move up";
-          "${mod}+Shift+l" = "move right";
+            "${mod}+Shift+h" = "move left";
+            "${mod}+Shift+j" = "move down";
+            "${mod}+Shift+k" = "move up";
+            "${mod}+Shift+l" = "move right";
 
-          "${mod}+Shift+Left" = "move left";
-          "${mod}+Shift+Down" = "move down";
-          "${mod}+Shift+Up" = "move up";
-          "${mod}+Shift+Right" = "move right";
+            "${mod}+Shift+Left" = "move left";
+            "${mod}+Shift+Down" = "move down";
+            "${mod}+Shift+Up" = "move up";
+            "${mod}+Shift+Right" = "move right";
 
-          "${mod}+v" = "split h"; # TODO send notification
-          "${mod}+g" = "split v"; # TODO send notification
-          "${mod}+q" = "split toggle";
+            "${mod}+v" = "split h"; # TODO send notification
+            "${mod}+g" = "split v"; # TODO send notification
+            "${mod}+q" = "split toggle";
 
-          "${mod}+Shift+space" = "floating toggle";
-          "${mod}+Shift+t" = "floating toggle";
+            "${mod}+Shift+space" = "floating toggle";
+            "${mod}+Shift+t" = "floating toggle";
 
-          "${mod}+Shift+1" = "move container to workspace $ws1; workspace $ws1";
-          "${mod}+Shift+2" = "move container to workspace $ws2; workspace $ws2";
-          "${mod}+Shift+3" = "move container to workspace $ws3; workspace $ws3";
-          "${mod}+Shift+4" = "move container to workspace $ws4; workspace $ws4";
-          "${mod}+Shift+5" = "move container to workspace $ws5; workspace $ws5";
-          "${mod}+Shift+6" = "move container to workspace $ws6; workspace $ws6";
-          "${mod}+Shift+7" = "move container to workspace $ws7; workspace $ws7";
-          "${mod}+Shift+8" = "move container to workspace $ws8; workspace $ws8";
-          "${mod}+Shift+9" = "move container to workspace $ws9; workspace $ws9";
-        };
+            "${mod}+Shift+1" = "move container to workspace $ws1; workspace $ws1";
+            "${mod}+Shift+2" = "move container to workspace $ws2; workspace $ws2";
+            "${mod}+Shift+3" = "move container to workspace $ws3; workspace $ws3";
+            "${mod}+Shift+4" = "move container to workspace $ws4; workspace $ws4";
+            "${mod}+Shift+5" = "move container to workspace $ws5; workspace $ws5";
+            "${mod}+Shift+6" = "move container to workspace $ws6; workspace $ws6";
+            "${mod}+Shift+7" = "move container to workspace $ws7; workspace $ws7";
+            "${mod}+Shift+8" = "move container to workspace $ws8; workspace $ws8";
+            "${mod}+Shift+9" = "move container to workspace $ws9; workspace $ws9";
+          };
       };
       extraConfig = ''
         set $ws1 1
