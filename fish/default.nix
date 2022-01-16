0ff3d4bc9a791370bd3767c1abe6b10fa18e2e4f
Tristan FLOCH <tristan.floch@epita.fr>
2022-01-16 14:14:44 +0100

    fishPkgs: fish-async-prompt and z

--- fish/default.nix
+++ fish/default.nix
@@ -42,6 +42,24 @@
           sha256 = "07kz44ln75n4r04wyks1838nhmhr7jqmsc1rh7am7glq9ja9inmx";
         };
       }
+      {
+        name = "z";
+        src = pkgs.fetchFromGitHub {
+          owner = "jethrokuan";
+          repo = "z";
+          rev = "45a9ff6d0932b0e9835cbeb60b9794ba706eef1";
+          sha256 = "1kjyl4gx26q8175wcizvsm0jwhppd00rixdcr1p7gifw6s308sd5";
+        };
+      }
+      {
+        name = "fish-async-prompt";
+        src = pkgs.fetchFromGitHub {
+          owner = "acomagu";
+          repo = "fish-async-prompt";
+          rev = "40f30a4048b81f03fa871942dcb1671ea0fe7a53";
+          sha256 = "19i59145lsjmidqlgk2dmvs3vg2m3zlz2rcms2kyyk1m3y63q8xi";
+        };
+      }
     ];
     functions = {
       ls = "exa $argv";
