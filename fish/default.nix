9b740d60fa60ac366b2b91b5e92ac2cf14651b50
Tristan FLOCH <tristan.floch@epita.fr>
2022-02-04 12:39:18 +0100

    fish: git abbr and unfunctional plugin

--- fish/default.nix
+++ fish/default.nix
@@ -18,22 +18,35 @@
       gs = "git stage";
       gu = "git restore --staged";
       ga = "git add";
+      gC = "git clone";
       gc = "git commit";
       gcf = "git commit --fixup";
       gcs = "git commit --squash";
+      gl = "git log --oneline --graph";
       gla = "git log --oneline --graph --all";
       gg = "git status";
       gr = "git rebase";
+      gri = "git rebase --interactive";
       gp = "git push";
       gpu = "git push --set-upstream origin";
       gp-f = "git push --force-with-lease";
+      gf = "git fetch";
       gF = "git pull";
       gb = "git branch";
+      gbb = "git switch";
       gbc = "git checkout -b";
       gbl = "git checkout";
       gbx = "git branch -d";
+      go = "git reset";
+      gOh = "git reset --hard";
+      gOs = "git reset --soft";
     };
     plugins = [
+      {
+        # FIXME
+        name = "done";
+        src = pkgs.fishPlugins.done;
+      }
       {
         name = "dracula";
         src = pkgs.fetchFromGitHub {
