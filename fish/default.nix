c05e2f7a93a7a8cc9c7908396581fa27968bab3b
Tristan FLOCH <tristan.floch@epita.fr>
2022-02-04 12:47:05 +0100

    fish: move gitAbbrs to separate file

--- fish/default.nix
+++ fish/default.nix
@@ -13,34 +13,7 @@
       apply = "~/.config/nixpkgs/apply.sh";
 
       flow = "nix run .#check-workflow";
-
-      g = "git";
-      gs = "git stage";
-      gu = "git restore --staged";
-      ga = "git add";
-      gC = "git clone";
-      gc = "git commit";
-      gcf = "git commit --fixup";
-      gcs = "git commit --squash";
-      gl = "git log --oneline --graph";
-      gla = "git log --oneline --graph --all";
-      gg = "git status";
-      gr = "git rebase";
-      gri = "git rebase --interactive";
-      gp = "git push";
-      gpu = "git push --set-upstream origin";
-      gp-f = "git push --force-with-lease";
-      gf = "git fetch";
-      gF = "git pull";
-      gb = "git branch";
-      gbb = "git switch";
-      gbc = "git checkout -b";
-      gbl = "git checkout";
-      gbx = "git branch -d";
-      go = "git reset";
-      gOh = "git reset --hard";
-      gOs = "git reset --soft";
-    };
+    } // import ./gitAbbrs.nix;
     plugins = [
       {
         # FIXME
