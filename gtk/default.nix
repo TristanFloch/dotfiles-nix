9279df06863b336348d72e1e12998faa08179dd2
Tristan FLOCH <tristan.floch@epita.fr>
2022-02-28 14:04:32 +0100

    cursor theme

--- gtk/default.nix
+++ gtk/default.nix
@@ -11,6 +11,12 @@
     iconTheme.name = "Dracula";
   };
 
+  xsession.pointerCursor = {
+    package = pkgs.xorg.xcursorthemes;
+    name = "Dracula-cursors";
+    size = 16;
+  };
+
   home.file = {
     ".icons".source = ./icons;
     ".icons".recursive = true;
