d0028f5c22ca32155a43576f82fc33b3b1723d87
Tristan FLOCH <tristan.floch@epita.fr>
2022-03-07 14:10:52 +0100

    add chemacs desktop entries

--- emacs/default.nix
+++ emacs/default.nix
@@ -20,6 +20,8 @@
     package = pkgs.emacsGcc;
   };
 
+  services.emacs.client.enable = false;
+
   home.packages = with pkgs; [
     pinentry-emacs
     emacs-all-the-icons-fonts
@@ -30,4 +32,37 @@
   ];
 
   services.gpg-agent.pinentryFlavor = "emacs";
+
+  xdg.desktopEntries =
+    let
+      commonOptions = {
+        genericName = "Text Editor";
+        comment = "Edit text";
+        mimeType = [ "text/english" "text/plain" "text/x-makefile" "text/x-c++hdr" "text/x-c++src" "text/x-chdr" "text/x-csrc" "text/x-java" "text/x-moc" "text/x-pascal" "text/x-tcl" "text/x-tex" "application/x-shellscript" "text/x-c" "text/x-c++" ];
+        categories = [ "Development" "TextEditor" ];
+        terminal = false;
+        # settings = {
+        #   StartupWmClass = "Emacs"; # FIXME
+        # };
+      };
+    in
+    {
+      doom-emacs = {
+        name = "Doom Emacs";
+        exec = "emacs --with-profile doom";
+        icon = "doom";
+      } // commonOptions;
+
+      nano-emacs = {
+        name = "NANO Emacs";
+        exec = "emacs --with-profile nano";
+        icon = "emacs"; # TODO
+      } // commonOptions;
+
+      gnu-emacs = {
+        name = "GNU Emacs";
+        exec = "emacs --with-profile gnu";
+        icon = "emacs";
+      } // commonOptions;
+    };
 }
