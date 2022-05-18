ce4684782204bc53ece37544990b1042a4c0d6d9
Tristan FLOCH <tristan.floch@epita.fr>
2022-05-18 18:30:25 +0200

    refactor: gtk dracula theme

--- home/themes/gtk/default.nix
+++ home/themes/gtk/default.nix
@@ -1,19 +1,25 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    dracula-theme
-  ];
-
   gtk = {
     enable = true;
-    theme.name = "Dracula";
-    iconTheme.name = "Dracula";
+    theme = {
+      name = "Dracula";
+      package = pkgs.dracula-theme;
+    };
+
+    iconTheme = { name = "Dracula"; };
+
     font = {
       name = "Ubuntu";
       size = 11;
     };
   };
+
+  # home.file.".icons".source = pkgs.fetchzip {
+  #   url = "https://github.com/dracula/gtk/files/5214870/Dracula.zip";
+  #   sha256 = "sha256-rcSKlgI3bxdh4INdebijKElqbmAfTwO+oEt6M2D1ls0=";
+  # };
 
   home.file = {
     ".icons".source = ./icons;
