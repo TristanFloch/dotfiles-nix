06042eb4cc4ff04feccd516113f4b4889e2b40be
Tristan FLOCH <tristan.floch@epita.fr>
2022-06-28 14:47:41 +0200

    sway: wlr and gtk portals

--- modules/desktop/sessions/wayland/sway/default.nix
+++ modules/desktop/sessions/wayland/sway/default.nix
@@ -14,8 +14,10 @@ in
     xdg.portal = {
       enable = true;
       wlr.enable = true;
-      # gtk portal needed to make gtk apps happy
-      extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-wlr
+        xdg-desktop-portal-gtk
+      ];
       gtkUsePortal = true;
     };
 
