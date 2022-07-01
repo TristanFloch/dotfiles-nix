b269f51b456ef5e0d1215b4d74ad49914ed578d6
Tristan FLOCH <tristan.floch@epita.fr>
2022-07-01 14:50:04 +0200

    fix dracula cursor for all wayland windows

--- home/themes/default.nix
+++ home/themes/default.nix
@@ -1,27 +1,14 @@
 { config, lib, pkgs, ... }:
 
-let
-  xsession = config.modules.desktop.sessions.x;
-in
-{
-  imports = [
-    ./gtk
-    ./font
-  ];
+let xsession = config.modules.desktop.sessions.x;
+in {
+  imports = [ ./gtk ./font ];
 
-  config = lib.mkIf xsession.enable {
-    # FIXME stoped working after update
-    home.pointerCursor = {
-      package = pkgs.xorg.xcursorthemes;
-      name = "Dracula-cursors";
-      gtk.enable = true;
-      x11.enable = true;
-      size = 16;
-    };
-    gtk.cursorTheme = {
-      package = pkgs.xorg.xcursorthemes;
-      name = "Dracula-cursors";
-      size = 16;
-    };
+  home.pointerCursor = {
+    name = "Dracula-cursors";
+    package = pkgs.xorg.xcursorthemes;
+    gtk.enable = true;
+    x11.enable = xsession.enable;
+    size = 16;
   };
 }
