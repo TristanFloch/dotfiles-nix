ab0a90af7345e488a4af07d5a97ad0aeb4eb71ff
Tristan FLOCH <tristan.floch@epita.fr>
2022-07-04 18:33:59 +0200

    fix(gtk): custom package for Dracula theme

--- home/themes/gtk/default.nix
+++ home/themes/gtk/default.nix
@@ -1,8 +1,52 @@
 { config, lib, pkgs, ... }:
 
 let
-  inherit (lib) mkIf;
-  xsession = config.modules.desktop.sessions.x;
+  dracula-icons = pkgs.stdenv.mkDerivation {
+    pname = "dracula-icons";
+    version = "1.0";
+
+    src = pkgs.fetchzip {
+      url = "https://github.com/dracula/gtk/files/5214870/Dracula.zip";
+      sha256 = "sha256-rcSKlgI3bxdh4INdebijKElqbmAfTwO+oEt6M2D1ls0=";
+    };
+
+    propagatedUserEnvPkgs = with pkgs; [
+      gtk-engine-murrine
+    ];
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/share/icons/Dracula
+      mv * $out/share/icons/Dracula
+
+      runHook postInstall
+    '';
+  };
+
+  dracula-icons-bis = pkgs.stdenv.mkDerivation rec {
+    pname = "dracula-icons";
+    version = "1.0";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "m4thewz";
+      repo = pname;
+      rev = "2d3c83caa8664e93d956cfa67a0f21418b5cdad8";
+      sha256 = "sha256-GY+XxTM22jyNq8kaB81zNfHRhfXujArFcyzDa8kjxCQ=";
+    };
+
+    dontPatchELF = true;
+    dontRewriteSymlinks = true;
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/share/icons/Dracula
+      mv * $out/share/icons/Dracula
+
+      runHook postInstall
+    '';
+  };
 in
 {
   gtk = {
@@ -13,9 +57,11 @@ in
     };
 
     iconTheme = {
-      # name = "Dracula";
-      name = "Tela-purple-dark";
-      package = pkgs.tela-icon-theme;
+      name = "Dracula";
+      package = dracula-icons-bis;
+
+      # name = "Tela-purple-dark";
+      # package = pkgs.tela-icon-theme;
     };
 
     font = {
@@ -23,14 +69,4 @@ in
       size = 11;
     };
   };
-
-  # home.file.".icons".source = pkgs.fetchzip {
-  #   url = "https://github.com/dracula/gtk/files/5214870/Dracula.zip";
-  #   sha256 = "sha256-rcSKlgI3bxdh4INdebijKElqbmAfTwO+oEt6M2D1ls0=";
-  # };
-
-  # home.file = {
-  #   ".icons".source = ./icons;
-  #   ".icons".recursive = true;
-  # };
 }
