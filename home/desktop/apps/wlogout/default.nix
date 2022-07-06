7ea6bb30ae404c1acd58fab4dc01dc37e6dd1285
Tristan FLOCH <tristan.floch@epita.fr>
2022-07-06 21:21:58 +0200

    refacto(wlogout): enable for for wayland session

--- home/desktop/apps/wlogout/default.nix
+++ home/desktop/apps/wlogout/default.nix
@@ -1,12 +1,17 @@
 { config, lib, pkgs, ... }:
 
-{
-  xdg = {
-    configFile."wlogout/layout".source = ./layout;
-    configFile."wlogout/style.css".source = ./style.css;
-    configFile."wlogout/icons" = {
-      source = ./icons;
-      recursive = true;
+let wayland = config.modules.desktop.sessions.wayland;
+in {
+  config = lib.mkIf wayland.enable {
+    home.packages = with pkgs; [ wlogout ];
+
+    xdg = {
+      configFile."wlogout/layout".source = ./layout;
+      configFile."wlogout/style.css".source = ./style.css;
+      configFile."wlogout/icons" = {
+        source = ./icons;
+        recursive = true;
+      };
     };
   };
 }
