3a00f2e5c0595c608f50c3214c0709a3844e7f41
Tristan FLOCH <tristan.floch@epita.fr>
2022-07-21 16:32:11 +0200

    fix(emacs): configure emacsWithPackge so vterm works

--- home/editors/emacs/default.nix
+++ home/editors/emacs/default.nix
@@ -1,26 +1,15 @@
 { config, lib, pkgs, ... }:
 
-# FIXME: problem in nix-doom-emacs
-# let
-#   doom-emacs = pkgs.callPackage (builtins.fetchTarball {
-#     url = https://github.com/vlaci/nix-doom-emacs/archive/develop.tar.gz;
-#   }) {
-#     doomPrivateDir = ./doom.d;
-#   };
-# in {
-#   home.packages = [ doom-emacs ];
-#   home.file.".emacs.d/init.el".text = ''
-#     (load "default.el")
-#   '';
-# }
-
-{
-  programs.emacs = let wayland = config.modules.desktop.sessions.wayland;
-  in {
+let
+  wayland = config.modules.desktop.sessions.wayland;
+  myEmacs =
+    if wayland.enable then pkgs.emacsPgtkNativeComp else pkgs.emacsNativeComp;
+  myEmacsWithPkgs = (pkgs.emacsPackagesFor myEmacs).emacsWithPackages
+    (epkgs: with epkgs; [ vterm ]);
+in {
+  programs.emacs = {
     enable = true;
-    package = if wayland.enable then
-      pkgs.emacsPgtkNativeComp else
-      pkgs.emacsNativeComp;
+    package = myEmacsWithPkgs;
   };
 
   services.emacs.client.enable = false;
@@ -28,7 +17,6 @@
   home.packages = with pkgs; [
     pinentry-emacs
     emacs-all-the-icons-fonts
-    bear
     (aspellWithDicts (dicts: with dicts; [ en en-computers en-science ]))
     shellcheck
     rnix-lsp
@@ -36,6 +24,7 @@
     rtags
     xclip
     fd
+    python3Full
   ];
 
   services.gpg-agent.pinentryFlavor = "emacs";
