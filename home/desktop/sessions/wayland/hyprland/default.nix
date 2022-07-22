8411954d4171372bcad3b22908fc1586619e0dc9
Tristan FLOCH <tristan.floch@epita.fr>
2022-07-22 16:05:57 +0200

    style(hyprland): format default.nix

--- home/desktop/sessions/wayland/hyprland/default.nix
+++ home/desktop/sessions/wayland/hyprland/default.nix
@@ -4,23 +4,24 @@ let
   inherit (lib) mkIf mkEnableOption;
   cfg = config.modules.desktop.sessions.wayland.hyprland;
   hyprctl = "${pkgs.hyprland}/bin/hyprctl";
-  handle_monitor_connect = pkgs.writeShellScriptBin "handle_monitor_connect.sh" ''
-    function handle {
-      if [ ''${1:0:12} = "monitoradded" ]; then
-        ${hyprctl} dispatch moveworkspacetomonitor "1 1"
-        ${hyprctl} dispatch moveworkspacetomonitor "2 2"
-        ${hyprctl} dispatch moveworkspacetomonitor "3 1"
-        ${hyprctl} dispatch moveworkspacetomonitor "4 2"
-        ${hyprctl} dispatch moveworkspacetomonitor "5 1"
-        ${hyprctl} dispatch moveworkspacetomonitor "6 2"
-        ${hyprctl} dispatch moveworkspacetomonitor "7 1"
-        ${hyprctl} dispatch moveworkspacetomonitor "8 2"
-        ${hyprctl} dispatch moveworkspacetomonitor "9 1"
-      fi
-    }
+  handle_monitor_connect =
+    pkgs.writeShellScriptBin "handle_monitor_connect.sh" ''
+      function handle {
+        if [ ''${1:0:12} = "monitoradded" ]; then
+          ${hyprctl} dispatch moveworkspacetomonitor "1 1"
+          ${hyprctl} dispatch moveworkspacetomonitor "2 2"
+          ${hyprctl} dispatch moveworkspacetomonitor "3 1"
+          ${hyprctl} dispatch moveworkspacetomonitor "4 2"
+          ${hyprctl} dispatch moveworkspacetomonitor "5 1"
+          ${hyprctl} dispatch moveworkspacetomonitor "6 2"
+          ${hyprctl} dispatch moveworkspacetomonitor "7 1"
+          ${hyprctl} dispatch moveworkspacetomonitor "8 2"
+          ${hyprctl} dispatch moveworkspacetomonitor "9 1"
+        fi
+      }
 
-    ${pkgs.socat}/bin/socat - UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock | while read line; do handle $line; done
-  '';
+      ${pkgs.socat}/bin/socat - UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock | while read line; do handle $line; done
+    '';
 in {
   options.modules.desktop.sessions.wayland.hyprland.enable =
     mkEnableOption "Hyprland";
