fd9b0449685a3806f13643efd7427b2a4510c17b
Tristan FLOCH <tristan.floch@epita.fr>
2022-09-18 18:38:07 +0200

    feat(nix): add trusted-users explicitly

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -22,6 +22,7 @@ in {
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
+      trusted-users = [ "root" "@wheel" ];
     };
 
     # Add each flake input as a registry
