3090e3f00584791fe2a1e4fc0c9c95262b1b539d
Tristan FLOCH <tristan.floch@epita.fr>
2022-09-18 18:36:58 +0200

    refactor(nix): build NIX_PATH dynamically

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -23,16 +23,14 @@ in {
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
-    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
-    nixPath = [
-      "nixpkgs=/etc/nix/channels/nixpkgs"
-      "home-manager=/etc/nix/channels/home-manager"
-    ];
-  };
 
-  environment.etc = {
-    "nix/channels/nixpkgs".source = inputs.nixpkgs.outPath;
-    "nix/channels/home-manager".source = inputs.home-manager.outPath;
+    # Add each flake input as a registry
+    # To make nix3 commands consistent with the flake
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+
+    # Map registries to channels
+    # Very useful when using legacy commands
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
   };
 
   # Use the systemd-boot EFI boot loader.
