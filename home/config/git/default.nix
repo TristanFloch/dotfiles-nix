ae73f540d00ec21900b9e40aa96ba0a8a53414f0
Tristan FLOCH <tristan.floch@epita.fr>
2022-09-20 19:04:34 +0200

    feat(git): sign by default

--- home/config/git/default.nix
+++ home/config/git/default.nix
@@ -3,5 +3,9 @@
     enable = true;
     userName = "Tristan FLOCH";
     userEmail = "tristan.floch@epita.fr";
+    signing = {
+      key = "204D153E08EE4DD9764D813CF762D9933B1FAAF5";
+      signByDefault = true;
+    };
   };
 }
