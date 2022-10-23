165c7e8d268eabb3722be74746e29c2da939191d
Tristan FLOCH <tristan.floch@epita.fr>
2022-10-23 20:02:34 +0200

    feat(thunar): add gvfs service

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -1,14 +1,7 @@
 { options, config, lib, pkgs, ... }:
 
-let
-  inherit (lib) mkIf mkEnableOption;
-  cfg = config.modules.services.docker;
-in {
-  options.modules.services.docker.enable = mkEnableOption "Docker";
+{
+  imports = [ ./docker ];
 
-  config = mkIf cfg.enable {
-    virtualisation.docker.enable = true;
-
-    environment.systemPackages = with pkgs; [ docker ];
-  };
+  services.gvfs.enable = true;
 }
