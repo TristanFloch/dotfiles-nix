725b778f9031093f49f63a0f5bc902ceadff1cd4
Tristan Floch <tristan.floch@gmail.com>
2023-07-25 16:23:03 +0200

    emacs: add home-manager commands to .dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((projectile-project-install-cmd . "home-manager --flake .#tristan@nixos-zenbook switch")
+         (projectile-project-compilation-cmd . "home-manager --flake .#tristan@nixos-zenbook build")
+         (projectile-project-test-cmd . "home-manager --flake .#tristan@nixos-zenbook test"))))
