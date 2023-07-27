6638c4717625ebcc0da74b9dfa7b308ec3a91b7d
Tristan Floch <tristan.floch@gmail.com>
2023-07-27 14:19:16 +0200

    remove unexisting home-manager cmd from dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,5 @@
 ((nil . ((projectile-project-install-cmd . "home-manager --flake .#tristan@nixos-zenbook switch")
          (projectile-project-compilation-cmd . "home-manager --flake .#tristan@nixos-zenbook build")
-         (projectile-project-test-cmd . "home-manager --flake .#tristan@nixos-zenbook test")
          (eval . (map! :leader
                        :prefix "p"
                        :desc "Apply home-manager config" "I" 'projectile-install-project)))))
