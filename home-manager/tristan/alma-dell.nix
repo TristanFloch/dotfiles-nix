4ec98d5bd81155f2ffdb8330d93defed64e1ffa8
Tristan Floch <tristan.floch@digeiz.com>
2024-07-12 16:12:23 +0200

    chore(alma-dell): switch to gcc-toolset-12

--- home-manager/tristan/alma-dell.nix
+++ home-manager/tristan/alma-dell.nix
@@ -37,7 +37,7 @@
     # enableCompletion = true;
     enableVteIntegration = true;
     profileExtra = ''
-      source scl_source enable gcc-toolset-11
+      source scl_source enable gcc-toolset-12
 
       export BOOST_INCLUDEDIR=/usr/include/boost169
       export BOOST_LIBRARYDIR=/usr/lib64/boost169
