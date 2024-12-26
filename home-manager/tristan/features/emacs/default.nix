cbc7b1e87d8e01ca17575e3c170fc6404bea86de
Tristan Floch <tristan.floch@digeiz.com>
2024-12-26 14:36:42 +0100

    feat(emacs): add nodejs to configuration packages

--- home-manager/tristan/features/emacs/default.nix
+++ home-manager/tristan/features/emacs/default.nix
@@ -1,15 +1,17 @@
 { config, lib, pkgs, ... }:
 
 let
-  myEmacsWithPkgs = (pkgs.emacsPackagesFor pkgs.emacs29).emacsWithPackages
-    (epkgs: with epkgs; [ vterm autothemer ]);
-  # myTex = (pkgs.texlive.combine {
-  #   inherit (pkgs.texlive)
-  #     scheme-medium dvisvgm dvipng wrapfig
-  #     capt-of # amsmath ulem hyperref  latexmk
-  #   ;
-  # });
-in rec {
+  myEmacsWithPkgs = (pkgs.emacsPackagesFor pkgs.emacs29).emacsWithPackages (
+    epkgs: with epkgs; [ vterm ]
+  );
+in
+# myTex = (pkgs.texlive.combine {
+#   inherit (pkgs.texlive)
+#     scheme-medium dvisvgm dvipng wrapfig
+#     capt-of # amsmath ulem hyperref  latexmk
+#   ;
+# });
+rec {
   programs.emacs = {
     enable = true;
     package = myEmacsWithPkgs;
@@ -20,13 +22,21 @@ in rec {
   home.packages = with pkgs; [
     pinentry-emacs
     emacs-all-the-icons-fonts
-    (aspellWithDicts (dicts: with dicts; [ en en-computers en-science fr ]))
+    (aspellWithDicts (
+      dicts: with dicts; [
+        en
+        en-computers
+        en-science
+        fr
+      ]
+    ))
     shellcheck
     shfmt
     nixfmt-rfc-style
     rtags
     xclip
     fd
+    nodejs
     # python311Packages.python-lsp-server
     # python3Full
     # poetry
@@ -35,54 +45,59 @@ in rec {
 
   # home.file.".icons/doom.png".source = ./doom.png;
 
-  xdg.desktopEntries = let
-    commonOptions = {
-      genericName = "Text Editor";
-      comment = "Edit text";
-      mimeType = [
-        "text/english"
-        "text/plain"
-        "text/x-makefile"
-        "text/x-c++hdr"
-        "text/x-c++src"
-        "text/x-chdr"
-        "text/x-csrc"
-        "text/x-java"
-        "text/x-moc"
-        "text/x-pascal"
-        "text/x-tcl"
-        "text/x-tex"
-        "application/x-shellscript"
-        "text/x-c"
-        "text/x-c++"
-      ];
-      categories = [ "Development" "TextEditor" ];
-      terminal = false;
+  xdg.desktopEntries =
+    let
+      commonOptions = {
+        genericName = "Text Editor";
+        comment = "Edit text";
+        mimeType = [
+          "text/english"
+          "text/plain"
+          "text/x-makefile"
+          "text/x-c++hdr"
+          "text/x-c++src"
+          "text/x-chdr"
+          "text/x-csrc"
+          "text/x-java"
+          "text/x-moc"
+          "text/x-pascal"
+          "text/x-tcl"
+          "text/x-tex"
+          "application/x-shellscript"
+          "text/x-c"
+          "text/x-c++"
+        ];
+        categories = [
+          "Development"
+          "TextEditor"
+        ];
+        terminal = false;
+      };
+      emacs = "${programs.emacs.package}/bin/emacs";
+    in
+    {
+      doom-emacs = {
+        name = "Doom Emacs";
+        exec = "${emacs} --init-directory ${config.xdg.configHome}/emacs-doom";
+        icon = ./doom.png;
+      } // commonOptions;
+
+      # nano-emacs = {
+      #   name = "NANO Emacs";
+      #   exec = "${emacs} --init-directory ${homeDir}/.emacs.d.nano";
+      #   icon = "emacs"; # TODO
+      # } // commonOptions;
+
+      # gnu-emacs = {
+      #   name = "GNU Emacs";
+      #   exec = "${emacs} --init-directory ${homeDir}/.emacs.d.gnu";
+      #   icon = "emacs";
+      # } // commonOptions;
+
+      emacs-minimal = {
+        name = "Emacs (Minimal)";
+        exec = "${emacs} --init-directory ${config.xdg.configHome}/emacs-minimal";
+        icon = "emacs";
+      } // commonOptions;
     };
-    emacs = "${programs.emacs.package}/bin/emacs";
-  in {
-    doom-emacs = {
-      name = "Doom Emacs";
-      exec = "${emacs} --init-directory ${config.xdg.configHome}/emacs-doom";
-      icon = ./doom.png;
-    } // commonOptions;
-
-    # nano-emacs = {
-    #   name = "NANO Emacs";
-    #   exec = "${emacs} --init-directory ${homeDir}/.emacs.d.nano";
-    #   icon = "emacs"; # TODO
-    # } // commonOptions;
-
-    # gnu-emacs = {
-    #   name = "GNU Emacs";
-    #   exec = "${emacs} --init-directory ${homeDir}/.emacs.d.gnu";
-    #   icon = "emacs";
-    # } // commonOptions;
-
-    emacs-minimal = {
-      name = "Emacs (Minimal)";
-      exec = "${emacs} --init-directory ${config.xdg.configHome}/emacs-minimal";
-      icon = "emacs";
-    } // commonOptions;
-  };
 }
