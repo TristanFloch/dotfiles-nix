1fb4cc6ae4fcfd12fffedc30cf8aaab06f341acd
Tristan Floch <tristan.floch@gmail.com>
2024-12-31 14:53:55 +0100

    feat(gnome): optionally enable forge extension for tiling

--- home-manager/tristan/features/desktop/gnome/default.nix
+++ home-manager/tristan/features/desktop/gnome/default.nix
@@ -9,18 +9,19 @@
 
       # `gnome-extensions list`
       enabled-extensions = [
-        "auto-move-windows@gnome-shell-extensions.gcampax.github.com"
         "drive-menu@gnome-shell-extensions.gcampax.github.com"
-        "focus-changer@heartmire"
         "gsconnect@andyholmes.github.io"
         "launch-new-instance@gnome-shell-extensions.gcampax.github.com"
         "pano@elhan.io"
         "screenshot-window-sizer@gnome-shell-extensions.gcampax.github.com"
         "trayIconsReloaded@selfmade.pl"
         "user-theme@gnome-shell-extensions.gcampax.github.com"
-        # "Vitals@CoreCoding.com"
+        "auto-move-windows@gnome-shell-extensions.gcampax.github.com"
+        "focus-changer@heartmire"
         "windowsNavigator@gnome-shell-extensions.gcampax.github.com"
-        "workspace-indicator@gnome-shell-extensions.gcampax.github.com"
+        # "Vitals@CoreCoding.com"
+        # "forge@jmmaranan.com"
+        # "workspace-indicator@gnome-shell-extensions.gcampax.github.com"
       ];
     };
   };
@@ -28,6 +29,7 @@
   home.packages = with pkgs.gnomeExtensions; [
     auto-move-windows
     focus-changer
+    forge
     gsconnect
     pano
     tray-icons-reloaded
