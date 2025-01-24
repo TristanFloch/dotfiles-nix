f6a434f764294bbcd54fca670893f866385156cb
Tristan Floch <tristan@macbook-pro.local>
2025-01-24 15:27:18 +0100

    Add a nix-darwin configuration

--- flake.nix
+++ flake.nix
@@ -2,90 +2,87 @@
   description = "Tristan's NixOS configuration";
 
   inputs = {
-    # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    # You can access packages and modules from different nixpkgs revs
-    # at the same time. Here's an working example:
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
-    # Home manager
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    systems.url = "github:nix-systems/default";
+
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin/nix-darwin-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # TODO: Add any other flake you might need
-    # hardware.url = "github:nixos/nixos-hardware";
-
-    # Shameless plug: looking for a way to nixify your themes and make
-    # everything match nicely? Try nix-colors!
-    # nix-colors.url = "github:misterio77/nix-colors";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, nix-darwin, home-manager, systems, ... }@inputs:
     let
       inherit (self) outputs;
-      forAllSystems = nixpkgs.lib.genAttrs [
-        # "aarch64-linux"
-        # "i686-linux"
-        "x86_64-linux"
-        # "aarch64-darwin"
-        # "x86_64-darwin"
-      ];
-    in
-    rec {
-      # Your custom packages
-      # Acessible through 'nix build', 'nix shell', etc
-      packages = forAllSystems (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in import ./pkgs { inherit pkgs; }
-      );
-      # Devshell for bootstrapping
-      # Acessible through 'nix develop' or 'nix-shell' (legacy)
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in import ./shell.nix { inherit pkgs; }
-      );
 
-      # Your custom packages and modifications, exported as overlays
-      overlays = import ./overlays { inherit inputs; };
-      # Reusable nixos modules you might want to export
-      # These are usually stuff you would upstream into nixpkgs
+      lib = nixpkgs.lib // nix-darwin.lib // home-manager.lib;
+      forEachSystem = f: lib.genAttrs (import systems) (system: f pkgsFor.${system});
+      pkgsFor = lib.genAttrs (import systems) (
+        system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        }
+      );
+    in {
+      # Custom packages acessible through 'nix build', 'nix shell', ...
+      packages = forEachSystem (pkgs: import ./pkgs {inherit pkgs;});
+
+      # Devshell for bootstrapping acessible through 'nix develop' or 'nix-shell' (legacy)
+      devShells = forEachSystem (pkgs: import ./shell.nix {inherit pkgs;});
+
+      formatter = forEachSystem (pkgs: pkgs.alejandra);
+
+      # Custom packages and modifications, exported as overlays
+      overlays = import ./overlays { inherit inputs outputs; };
+
+      # Reusable nixos modules to possibly export
+      # These are usually stuff to upstream into nixpkgs
       nixosModules = import ./modules/nixos;
-      # Reusable home-manager modules you might want to export
-      # These are usually stuff you would upstream into home-manager
+
+      # Reusable home-manager modules to possibly export
+      # These are usually stuff to upstream into home-manager
       homeManagerModules = import ./modules/home-manager;
 
-      # NixOS configuration entrypoint
-      # Available through 'nixos-rebuild --flake .#your-hostname'
+      # NixOS configurations entrypoint
+      # Available through 'nixos-rebuild --flake .#configName'
       nixosConfigurations = {
-        nixos-zenbook = nixpkgs.lib.nixosSystem {
+        nixosZenbook = lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
-          modules = [
-            ./nixos/nixos-zenbook
-          ];
+          modules = [ ./nixos/nixos-zenbook ];
+        };
+      };
+
+      # nix-darwin configurations entrypoint
+      # Available through 'darwin-rebuild --flake .#configName'
+      darwinConfigurations = {
+        macbookProM4 = lib.darwinSystem {
+          modules = [ ./darwin/macboook-pro-m4 ];
+          specialArgs = { inherit self inputs outputs; };
         };
       };
 
       # Standalone home-manager configurations entrypoint
       # Available through 'home-manager --flake .#your-username@your-hostname'
       homeConfigurations = {
-        "tristan@nixos-zenbook" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        "tristan@nixos-zenbook" = lib.homeManagerConfiguration {
+          pkgs = pkgsFor.x86_64-linux;
           extraSpecialArgs = { inherit inputs outputs; };
-          modules = [
-            # > Our main home-manager configuration file <
-            ./home-manager/tristan/nixos-zenbook.nix
-          ];
+          modules = [ ./home-manager/tristan/nixos-zenbook.nix ];
         };
 
-        "tfloch@alma-dell" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        "tfloch@alma-dell" = lib.homeManagerConfiguration {
+          pkgs = pkgsFor.x86_64-linux;
           extraSpecialArgs = { inherit inputs outputs; };
-          modules = [
-            ./home-manager/tristan/alma-dell.nix
-          ];
+          modules = [ ./home-manager/tristan/alma-dell.nix ];
         };
       };
     };
