d55cd758bae07908eb1dc22567648333f875ff63
Tristan Floch <tristan.floch@gmail.com>
2025-05-12 14:47:12 +0200

    build(emacs): update .dir-local to darwin commands

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
-((nil . ((projectile-project-install-cmd . "home-manager --flake .#tristan@nixos-zenbook switch")
-         (projectile-project-compilation-cmd . "home-manager --flake .#tristan@nixos-zenbook build")
+((nil . ((projectile-project-install-cmd . "darwin-rebuild --flake .# switch")
+         (projectile-project-compilation-cmd . "darwin-rebuild --flake .# build")
          (eval . (map! :leader
                        :prefix "p"
                        :desc "Apply home-manager config" "I" 'projectile-install-project)))))
