09e54516d892b3d25fae13ef5e98a361c08e8f6e
Tristan Floch <tristan.floch@algolia.com>
2025-10-14 11:01:38 +0200

    style(fish): apply formatting

--- home-manager/tristan/features/cli/fish/default.nix
+++ home-manager/tristan/features/cli/fish/default.nix
@@ -1,4 +1,9 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   programs.fish = {
@@ -38,34 +43,37 @@
       inherit pkgs;
     };
 
-    functions = let
-      batmanEnabled = config.programs.bat.enable
-        && builtins.elem pkgs.bat-extras.batman
-        config.programs.bat.extraPackages;
-    in {
-      ls = "${pkgs.eza}/bin/eza $argv";
-      lls = "${pkgs.coreutils}/bin/ls -f $argv"; # fast ls
-      cat = "${pkgs.bat}/bin/bat $argv";
-      ccat = "${pkgs.coreutils}/bin/cat $argv";
-      tree = "${pkgs.eza}/bin/eza --tree $argv";
-      ttree = "${pkgs.tree}/bin/tree $argv";
-      ex = {
-        body = builtins.readFile ./functions/ex.fish;
-        description = "extracts any archive format";
+    functions =
+      let
+        batmanEnabled =
+          config.programs.bat.enable
+          && builtins.elem pkgs.bat-extras.batman config.programs.bat.extraPackages;
+      in
+      {
+        ls = "${pkgs.eza}/bin/eza $argv";
+        lls = "${pkgs.coreutils}/bin/ls -f $argv"; # fast ls
+        cat = "${pkgs.bat}/bin/bat $argv";
+        ccat = "${pkgs.coreutils}/bin/cat $argv";
+        tree = "${pkgs.eza}/bin/eza --tree $argv";
+        ttree = "${pkgs.tree}/bin/tree $argv";
+        ex = {
+          body = builtins.readFile ./functions/ex.fish;
+          description = "extracts any archive format";
+        };
+        # monitor = {
+        #   body = builtins.readFile ./functions/monitor.fish;
+        #   description = "connects/disconnects pluged/unpluged monitors";
+        # };
+        fish_user_key_bindings = {
+          body = ''
+            fish_default_key_bindings -M insert
+            fish_vi_key_bindings --no-erase insert
+          '';
+        };
+      }
+      // lib.optionalAttrs batmanEnabled {
+        man = "${pkgs.bat-extras.batman}/bin/batman $argv";
       };
-      # monitor = {
-      #   body = builtins.readFile ./functions/monitor.fish;
-      #   description = "connects/disconnects pluged/unpluged monitors";
-      # };
-      fish_user_key_bindings = {
-        body = ''
-          fish_default_key_bindings -M insert
-          fish_vi_key_bindings --no-erase insert
-        '';
-      };
-    } // lib.optionalAttrs batmanEnabled {
-      man = "${pkgs.bat-extras.batman}/bin/batman $argv";
-    };
   };
 
   # programs.nix-index = {
